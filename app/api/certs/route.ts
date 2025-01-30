10362ecc2569d715bd0ea1d3dcccf1f426d5f91c
Adam <awhiles2011@gmail.com>
2025-01-30 07:45:59 -0600

    update cosmosdb endpoint

--- app/api/certs/route.ts
+++ app/api/certs/route.ts
@@ -4,9 +4,9 @@ const cosmosDb = new DbClient();
 
 export async function GET() {
   const client = await cosmosDb.getConnection();
-  const database = client.database("msftlearningresources");
-  const container = database.container("learningresources");
-  const query = container.items.query(`SELECT * FROM learningresoources`);
+  const database = client.database("certranker");
+  const container = database.container("certs");
+  const query = container.items.query(`SELECT * FROM certs`);
   const data = await query.fetchAll();
   return Response.json(data);
 }
